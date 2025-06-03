7bd1ae372d14b588f57c1678fd385425ebc1d129
Amal A M <vectorhero007@gmail.com>
2025-06-03 18:29:14 +0530

    ui phase 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,89 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Quizzy());
 }
 
+class Quizzy extends StatelessWidget {
+  const Quizzy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Text(
+                'The CPU is known as the brain of the computer.',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
+              child: Text(
+                'True',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
+              child: Text(
+                'False',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Icon(Icons.done, color: Colors.green),
+            Icon(Icons.clear, color: Colors.red),
+          ],
+        ),
+      ],
+    );
+  }
+}
