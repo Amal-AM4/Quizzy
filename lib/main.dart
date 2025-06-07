5757709c0e6e27a25b110847f06087937727e1d1
Amal A M <vectorhero007@gmail.com>
2025-06-07 11:09:29 +0530

    questionBank is updated

--- lib/main.dart
+++ lib/main.dart
@@ -40,33 +40,18 @@ class _QuizPageState extends State<QuizPage> {
     // Icon(Icons.clear, color: Colors.red),
   ];
 
-  // List<String> questions = [
-  //   'The `<script>` tag can only be placed in the `<head>` section of an HTML document.',
-  //   'JavaScript can be used to change the content of an HTML element.',
-  //   'JavaScript can be used to change the CSS styles of an HTML element.',
-  //   'To use an external JavaScript file, you use the `<link>` tag.',
-  //   'The `onclick` attribute in HTML is an example of an event handler.',
-  //   'JavaScript is case-sensitive.',
-  //   'Semicolons are always required at the end of every JavaScript statement.',
-  //   'HTML5 introduced new APIs that can be accessed with JavaScript, like `localStorage`.',
-  //   '`document.write()` is the recommended way to add content to a webpage after it has loaded.',
-  //   '`innerHTML` and `innerText` are interchangeable and always produce the same output.',
-  // ];
-
-  // List<bool> answers = [
-  //   false,
-  //   true,
-  //   true,
-  //   false,
-  //   true,
-  //   true,
-  //   false,
-  //   true,
-  //   false,
-  //   false,
-  // ];
-
-  Question q1 = Question(q: 'The `<script>` tag can only be placed in the `<head>` section of an HTML document.', a: false);
+  List<Question> questionBank = [
+    Question(q: 'The `<script>` tag can only be placed in the `<head>` section of an HTML document.', a: false),
+    Question(q: 'JavaScript can be used to change the content of an HTML element.', a: true),
+    Question(q: 'JavaScript can be used to change the CSS styles of an HTML element.', a: true),
+    Question(q: 'To use an external JavaScript file, you use the `<link>` tag.', a: false),
+    Question(q: 'The `onclick` attribute in HTML is an example of an event handler.', a: true),
+    Question(q: 'JavaScript is case-sensitive.', a: true),
+    Question(q: 'Semicolons are always required at the end of every JavaScript statement.', a: false),
+    Question(q: 'HTML5 introduced new APIs that can be accessed with JavaScript, like `localStorage`.', a: true),
+    Question(q: '`document.write()` is the recommended way to add content to a webpage after it has loaded.', a: false),
+    Question(q: '`innerHTML` and `innerText` are interchangeable and always produce the same output.', a: false),
+  ];
 
   int questionNumber = 0;
 
--- lib/question.dart
+++ lib/question.dart
@@ -2,6 +2,7 @@ class Question {
   late String questionText;
   late bool questionAnswer;
 
+  // constructor
   Question({required String q, required bool a}) {
     questionText = q;
     questionAnswer = a;
